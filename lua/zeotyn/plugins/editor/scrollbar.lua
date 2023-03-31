e449dad81e7b9f945707f47f8a8f5922c94496d8
Eugen Waldschmidt <eugen.waldschmidt@lhind.dlh.de>
2023-03-31 12:14:50 +0200

    Add scrollbar for better view of changes like in idea

---
+++ lua/zeotyn/plugins/editor/scrollbar.lua
@@ -0,0 +1,25 @@
+return {
+  "petertriho/nvim-scrollbar",
+  event = "BufReadPost",
+  opts = {
+    set_highlights = false,
+    excluded_filetypes = {
+      "prompt",
+      "TelescopePrompt",
+      "noice",
+      "neo-tree",
+      "dashboard",
+      "alpha",
+      "lazy",
+      "mason",
+      "DressingInput",
+      "",
+    },
+    handlers = {
+      gitsigns = true,
+    },
+  },
+  config = function()
+    require("scrollbar").setup()
+  end
+}
