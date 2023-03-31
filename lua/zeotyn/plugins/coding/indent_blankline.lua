ed1b59fbdba71d07e4e4e1efb693808c0398490f
Eugen Waldschmidt <eugen.waldschmidt@lhind.dlh.de>
2023-03-31 19:18:42 +0200

    Add colorizer and some blankline configs

--- lua/zeotyn/plugins/coding/indent_blankline.lua
+++ lua/zeotyn/plugins/coding/indent_blankline.lua
@@ -4,7 +4,9 @@ return {
     require("indent_blankline").setup {
       -- for example, context is off by default, use this to turn it on
       show_current_context = true,
-      show_current_context_start = true,
+      show_current_context_start = false,
+      use_treesitter = true,
+      use_treesitter_scope = true,
     }
   end
 }
