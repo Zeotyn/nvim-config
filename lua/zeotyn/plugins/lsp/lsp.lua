34a8ce4a5f94c416d5a5b7c7ea96d0307526b13a
Eugen Waldschmidt <eugen.waldschmidt@lhind.dlh.de>
2023-03-31 12:13:42 +0200

    Refactor cmp config to avoid using lsp-zero config

--- lua/zeotyn/plugins/lsp/lsp.lua
+++ lua/zeotyn/plugins/lsp/lsp.lua
@@ -31,7 +31,7 @@ return {
       set_lsp_keymaps = false,
       configure_diagnostics = true,
       cmp_capabilities = true,
-      manage_nvim_cmp = true,
+      manage_nvim_cmp = false,
       call_servers = 'local',
       sign_icons = {
         error = '✘',
@@ -66,36 +66,108 @@ return {
         })
     })
 
+    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+      vim.lsp.handlers.hover,
+      { border = nil }
+    )
+
+
     local luasnip = require('luasnip')
+    vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+    local cmp_select_opts = { behavior = cmp.SelectBehavior.Select }
+
     cmp.setup({
+      completion = {
+        completeopt = 'menu,menuone,noinsert'
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      formatting = {
+        fields = { 'abbr', 'menu', 'kind' },
+        format = function(entry, item)
+          local short_name = {
+            nvim_lsp = 'LSP',
+            nvim_lua = 'nvim'
+          }
+
+          local menu_name = short_name[entry.source.name] or entry.source.name
+
+          item.menu = string.format('[%s]', menu_name)
+          return item
+        end,
+      },
       sources = {
-        { name = "luasnip" },
+        { name = 'path' },
+        { name = 'nvim_lsp', keyword_length = 3 },
+        { name = 'buffer',   keyword_length = 3 },
+        { name = 'luasnip',  keyword_length = 2 },
+      },
+      window = {
       },
       mapping = {
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ["<CR>"] = cmp.mapping.confirm { select = true },
-        ["<Tab>"] = cmp.mapping(function(fallback)
+        -- confirm selection
+        ['<C-y>'] = cmp.mapping.confirm({ select = false }),
+        -- navigate items on the list
+        ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+        ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+        -- scroll up and down in the completion documentation
+        ['<C-d>'] = cmp.mapping.scroll_docs(5),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-5),
+        -- toggle completion
+        ['<C-e>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
+            cmp.abort()
+            fallback()
+          else
             cmp.complete()
+          end
+        end),
+        -- go to next placeholder in the snippet
+        ['<C-f>'] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(1) then
+            luasnip.jump(1)
           else
             fallback()
           end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
+        end, { 'i', 's' }),
+        -- go to previous placeholder in the snippet
+        ['<C-b>'] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(-1) then
             luasnip.jump(-1)
           else
             fallback()
           end
-        end, { "i", "s" }),
+        end, { 'i', 's' }),
+        -- when menu is visible, navigate to next item
+        -- when line is empty, insert a tab character
+        -- else, activate completion
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          local col = vim.fn.col('.') - 1
+
+          if cmp.visible() then
+            cmp.select_next_item(cmp_select_opts)
+          elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+            fallback()
+          else
+            cmp.complete()
+          end
+        end, { 'i', 's' }),
+        -- when menu is visible, navigate to previous item on list
+        -- else, revert to default behavior
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item(cmp_select_opts)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
       },
     })
 
@@ -152,6 +224,8 @@ return {
     })
   end
 }
+
+-- OLD Version from scratch
 -- return {
 --   'neovim/nvim-lspconfig',
 --   enable = false,
