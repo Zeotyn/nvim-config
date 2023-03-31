d6032a6eb7cd649598cb33a933f50be6f9bde801
Eugen Waldschmidt <eugen.waldschmidt@lhind.dlh.de>
2023-03-31 12:16:55 +0200

    Add buffeline configs for style

--- lua/zeotyn/plugins/editor/bufferline.lua
+++ lua/zeotyn/plugins/editor/bufferline.lua
@@ -5,20 +5,45 @@ return {
     require('bufferline').setup({
       options = {
         mode = 'buffers',
-        offsets = {
-          {filetype = 'neo-tree'}
+        diagnostics = "nvim_lsp",     -- | "nvim_lsp" | "coc",
+        -- separator_style = "slant", -- | "thick" | "thin" | "slope" | { 'any', 'any' },
+        separator_style = { "", "" }, -- | "thick" | "thin" | { 'any', 'any' },
+        indicator = {
+          -- icon = " ",
+          -- style = 'icon',
+          style = "underline",
         },
+        diagnostics_indicator = function(count, _, _, _)
+          if count > 9 then
+            return "9+"
+          end
+          return tostring(count)
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Project",
+            text_align = "center",
+            -- separator = true,
+          },
+        },
+        hover = {
+          enabled = true,
+          delay = 0,
+          reveal = { "close" },
+        },
+        tab_size = 25,
+        color_icons = true,
       },
       highlights = {
         buffer_selected = {
           italic = false
         },
         indicator_selected = {
-          fg = {attribute = 'fg', highlight = 'Function'},
+          fg = { attribute = 'fg', highlight = 'Function' },
           italic = false
         }
       }
     })
   end
 }
-
